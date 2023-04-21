7392307190a99ab8ec981d817cf375e6afad4075
Sergio <64972770+ssr765@users.noreply.github.com>
2023-04-21 19:03:37 +0200

    Primer commit de la implemetació en Python

---
+++ python_implementation/heapsort.py
@@ -0,0 +1,7 @@
+class HeapSort:
+    """Representra el HeapSort que ordenarà la llista."""
+    def __init__(self, llista: list) -> None:
+        pass
+
+    def heapify(self) -> None:
+        pass
---
+++ python_implementation/main.py
@@ -0,0 +1,7 @@
+from heapsort import HeapSort
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
