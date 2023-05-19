8a8878032c0f332e7bb701cc4b9bd151e917fe28
iv2424 <114505434+iv2424@users.noreply.github.com>
2023-05-19 17:41:47 +0200

    He corregit faltes d'ortografia de NoEsLlista.java

--- src/main/java/com/heapsort/exceptions/NoEsLlista.java
+++ src/main/java/com/heapsort/exceptions/NoEsLlista.java
@@ -1,7 +1,7 @@
 /**
- * Excepció que afloreix quan la llista no es de tipus ArrayList.
+ * Excepció que afloreix quan la llista no és de tipus ArrayList.
  * 
- * @author Sergi Serra, Yasmine Fadiss, Jessica Martinez, Arnau Vilalta, Iván Tovar, Pol Sanchez, Maximiliano Godoy
+ * @author Sergi Serra, Yasmine Fadiss, Jessica Martinez, Arnau Vilalta, Iván Tovar, Pol Sanchez, Maximiliano Godoy.
  * @version 1.0
  */
 package main.java.com.heapsort.exceptions;
@@ -13,6 +13,6 @@ public class NoEsLlista extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public NoEsLlista() {
-        super("El valor passat no es una llista de tipus ArrayList.");
+        super("El valor passat no és una llista de tipus ArrayList.");
     }
 }
