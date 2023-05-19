7d7fd2771ffb45da3c992a821bcec1497844c859
iv2424 <114505434+iv2424@users.noreply.github.com>
2023-05-19 18:18:23 +0200

    Update NoEsLlista.java

--- src/main/java/com/heapsort/exceptions/NoEsLlista.java
+++ src/main/java/com/heapsort/exceptions/NoEsLlista.java
@@ -1,7 +1,7 @@
 /**
  * Excepció que afloreix quan la llista no és de tipus ArrayList.
  * 
- * @author Sergi Serra, Yasmine Fadiss, Jessica Martinez, Arnau Vilalta, Iván Tovar, Pol Sanchez, Maximiliano Godoy.
+ * @author Sergi Serra, Yasmine Fadiss, Jessica Martinez, Arnau Vilalta, Iván Tovar, Pol Sanchez, Maximiliano Godoy
  * @version 1.0
  */
 package main.java.com.heapsort.exceptions;
