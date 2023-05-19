2533372bf0f5874a65df4544caad9a6102a672e0
iv2424 <114505434+iv2424@users.noreply.github.com>
2023-05-19 17:40:55 +0200

    He corregit faltes d'ortografia al LlistaBuida.java

--- src/main/java/com/heapsort/exceptions/LlistaBuida.java
+++ src/main/java/com/heapsort/exceptions/LlistaBuida.java
@@ -1,5 +1,5 @@
 /**
- * Excepció que afloreix quan la llista es buida.
+ * Excepció que afloreix quan la llista és buida.
  * 
  * @author Sergi Serra, Yasmine Fadiss, Jessica Martinez, Arnau Vilalta, Iván Tovar, Pol Sanchez, Maximiliano Godoy
  * @version 1.0
@@ -13,6 +13,6 @@ public class LlistaBuida extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public LlistaBuida() {
-        super("La lista es buida.");
+        super("La lista és buida.");
     }
 }
